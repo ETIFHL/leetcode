81191fe014ad6bf532e70aa4d9da5b7b62448aa7
etifhl <180253151@qq.com>
2020-06-30 21:53:36 +0800

    NestedInteger 结构

--- struct.js
+++ struct.js
@@ -2,3 +2,49 @@ function TreeNode(val) {
   this.val = val
   this.left = this.right = null
 }
+
+function NestedInteger() {
+  /**
+   *
+   Return true if this NestedInteger holds a single integer, rather than a nested list.
+   @return {boolean}
+   */
+  this.isInteger = function () {
+  }
+
+  /**
+   *
+   Return the single integer that this NestedInteger holds, if it holds a single integer
+   Return null if this NestedInteger holds a nested list
+   @return {number}
+   */
+  this.getInteger = function () {
+  }
+
+  /**
+   *
+   Set this NestedInteger to hold a single integer equal to value.
+   @return {void}
+   * @param value
+   */
+  this.setInteger = function (value) {
+  }
+
+  /**
+   *
+   Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+   @return {void}
+   * @param elem
+   */
+  this.add = function (elem) {
+  }
+
+  /**
+   *
+   Return the nested list that this NestedInteger holds, if it holds a nested list
+   Return null if this NestedInteger holds a single integer
+   @return {NestedInteger[]}
+   */
+  this.getList = function () {
+  }
+}
