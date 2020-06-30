555f27a1ec1142a229d227748bb5bd1489257f93
etifhl <180253151@qq.com>
2020-06-30 21:08:38 +0800

    扁平化嵌套列表迭代器

---
+++ stack/[341]扁平化嵌套列表迭代器.js
@@ -0,0 +1,90 @@
+//给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+//
+// 列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+//
+//
+//
+// 示例 1:
+//
+// 输入: [[1,1],2,[1,1]]
+//输出: [1,1,2,1,1]
+//解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+//
+// 示例 2:
+//
+// 输入: [1,[4,[6]]]
+//输出: [1,4,6]
+//解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+//
+// Related Topics 栈 设计
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @constructor
+ * @param {NestedInteger[]} nestedList
+ */
+var NestedIterator = function (nestedList) {
+  function getVal(nestNode, arr) {
+    if (nestNode.isInteger()) {
+      arr.push(nestNode.getInteger()) // 是int，直接计入
+    } else {
+      for (let i = 0; i < nestNode.getList().length; ++i) { // 遍历列表
+        getVal(nestNode.getList()[i], arr)
+      }
+    }
+  }
+
+  this.arr = [] // 直接扁平化一个辅助数组
+  for (let i = 0; i < nestedList.length; ++i) {
+    getVal(nestedList[i], this.arr)
+  }
+}
+
+/**
+ * @this NestedIterator
+ * @returns {boolean}
+ */
+NestedIterator.prototype.hasNext = function () {
+  return this.arr.length > 0
+}
+
+/**
+ * @this NestedIterator
+ * @returns {integer}
+ */
+NestedIterator.prototype.next = function () {
+  return this.arr.shift()
+}
+
+/**
+ * Your NestedIterator will be called like this:
+ * var i = new NestedIterator(nestedList), a = [];
+ * while (i.hasNext()) a.push(i.next());
+ */
+//leetcode submit region end(Prohibit modification and deletion)
