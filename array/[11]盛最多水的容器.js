59ebda771619ce6736ab660b4fa4b78d7a6a4230
etifhl <180253151@qq.com>
2020-07-01 23:48:19 +0800

    盛最多水的容器

--- array/[11]盛最多水的容器.js
+++ array/[11]盛最多水的容器.js
@@ -26,7 +26,7 @@
 var maxArea = function(height) {
   let max = 0
   for (let i = 0; i < height.length - 1; ++i) {
-    for (let j = i; j < height.length; ++j) {
+    for (let j = i + 1; j < height.length; ++j) {
       max = Math.max(max, Math.min(height[i], height[j]) * (j - i))
     }
   }
