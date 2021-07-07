c064b85dcc7703c0375e3a13eabe91ad52ebc4ec
ETIFHL <ashetifhl@gmail.com>
2021-07-07 22:36:59 +0800

    [64]最小路径和.js

---
+++ dp/[64]最小路径和.js
@@ -0,0 +1,40 @@
+//给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+//
+// 说明：每次只能向下或者向右移动一步。
+//
+// 示例:
+//
+// 输入:
+//[
+//  [1,3,1],
+//  [1,5,1],
+//  [4,2,1]
+//]
+//输出: 7
+//解释: 因为路径 1→3→1→1→1 的总和最小。
+//
+// Related Topics 数组 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+  const arr = [...grid[0]]
+  for (let i = 1; i < arr.length; ++i) {
+    arr[i] += arr[i - 1]
+  }
+  for(let i = 1; i < grid.length; ++i){
+    for(let j = 0; j < grid[0].length; ++j){
+      if(j == 0) {
+        arr[j] += grid[i][j]
+      } else {
+        arr[j] = Math.min(arr[j], arr[j-1]) + grid[i][j]
+      }
+    }
+  }
+  return arr[arr.length - 1]
+};
+//leetcode submit region end(Prohibit modification and deletion)
